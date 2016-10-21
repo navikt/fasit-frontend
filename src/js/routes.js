513132b2165d0da7c56b9b93c0b521cc7a76f322
Sundby, Frode <Frode.Sundby@nav.no>
2016-10-21 09:22:34 +0200

    Fjerner unødvendige routes og gjør om til route params

--- src/js/routes.js
+++ src/js/routes.js
@@ -7,16 +7,9 @@ import App from './components/Root/App'
 import Home from './components/Home'
 import Login from './components/Login'
 import Applications from './components/Applications/Applications'
-import Application from './components/Applications/Application'
-import ApplicationOverview from './components/Applications/Overview'
-import ApplicationInstances from './components/Applications/Instances'
-import ApplicationClusters from './components/Applications/Clusters'
-import Cluster from './components/Clusters/Cluster'
 import Nodes from './components/Nodes/Nodes'
 import Environments from './components/Environments/Environments'
 import Resources from './components/Resources/Resources'
-import Resource from './components/Resources/Resource'
-import Instance from './components/Instances/Instance'
 import Instances from './components/Instances/Instances'
 import NotFound from './components/Navigation/NotFound'
 
@@ -27,22 +20,9 @@ export default () => {
             <Route path="/login" component={Login}/>
             <Route path="/nodes(/:node)" component={Nodes}/>
             <Route path="/environments(/:environment)" component={Environments}/>
-
-            <Route path="/applications" component={Applications}/>
-            <Route path="/applications/:application" component={Application}>
-                <IndexRoute component={ApplicationOverview}/>
-                <Route path="/applications/:application" component={ApplicationOverview}/>
-                <Route path="/applications/:application/instances" component={ApplicationInstances}/>
-                <Route path="/applications/:application/clusters" component={ApplicationClusters}/>
-                <Route path="/applications/:application/instances/:instance" component={Instance}/>
-                <Route path="/applications/:application/clusters/:cluster" component={Cluster}/>
-            </Route>
-            <Route path="/resources" component={Resources}/>
-            <Route path="/resources/:resource" component={Resource}/>
-            <Route path="/instances" component={Instances}/>
-            <Route path="/instances/:instance" component={Instance}>
-                <IndexRoute component={ApplicationOverview}/>
-            </Route>
+            <Route path="/applications(/:application)" component={Applications}/>
+            <Route path="/resources(/:resource)" component={Resources}/>
+            <Route path="/instances(/:instance)" component={Instances}/>
             <Route path="*" component={NotFound}/>
         </Route>
     )
