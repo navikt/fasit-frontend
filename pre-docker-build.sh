84fcc2e9c39235ac25da4f720c3b70f85fa4bd88
Sundby, Frode <Frode.Sundby@nav.no>
2016-10-24 13:43:08 +0200

    Legger til pre-docker-build

---
+++ pre-docker-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOCKERDIR=$(dirname $0)/docker
+DISTDIR=${DOCKERDIR}/dist
+
+# prepares a docker directory for build
+rm -rf ${DOCKERDIR}
+mkdir -p ${DISTDIR}
+
+# init build-tool and build the frontend
+npm install && npm run build || exit 1
+
+# include frontend in dist
+cp -r dist ${DOCKERDIR}
+
+cp Dockerfile ${DOCKERDIR}
