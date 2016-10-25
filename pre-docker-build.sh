623edeb9b6a9b73a57c07dde05651e1751565222
Sundby, Frode <Frode.Sundby@nav.no>
2016-10-25 09:38:40 +0200

    Flytter unødvendige pakker for produksjon til devDependencies. Gjør om pre-docker-build

--- pre-docker-build.sh
+++ pre-docker-build.sh
@@ -7,10 +7,17 @@ DISTDIR=${DOCKERDIR}/dist
 rm -rf ${DOCKERDIR}
 mkdir -p ${DISTDIR}
 
+# Copy required files to the dist directory
+cp server.js config.js ${DISTDIR}
+
+# install the application dependencies
+cd ${DISTDIR} && cp ../../package.json . && npm install --production && cd -
+
 # init build-tool and build the frontend
 npm install && npm run build || exit 1
 
-# include frontend in dist
-cp -r dist ${DOCKERDIR}
+# Copy finished build to dist directory
+cp -r dist ${DISTDIR}
 
-cp Dockerfile server.js ${DOCKERDIR}
+# Copy Dockerfile in order to build from docker dir
+cp Dockerfile ${DOCKERDIR}
