2f90b52aa24283ad2d25f026788a4201d078b4cc
Frode Sundby <frodesun@gmail.com>
2016-11-27 15:20:29 +0100

    Ny fasitlogo og plassering av denne på hjem

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -15,60 +15,61 @@ class Home extends Component {
         const context = location.pathname.split('/')[1] || "anything"
         return (
             <div>
+                <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 hidden-xs text-center">
                 <div className="home-brand-logo-container">
-                    <div className="col-md-12">
-                        <img src="images/fasit-stempel.png" className="home-brand-logo "/>
-                    </div>
-                </div>
-                <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-                    <form>
-                        <input
-                            type="text"
-                            className="form-control search-field-text-input"
-                            ref="searchField"
-                            placeholder={'Search for ' + context}
-                            value={searchString}
-                            onChange={(e) => dispatch(submitSearchString(context, e.target.value, 0))}
-                        />
-                        <button
-                            type="submit"
-                            className="search-field-button btn-grey"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                browserHistory.push("/search")
-                            }}
-                        ><i className="fa fa-search"/></button>
-                    </form>
-                    <br />
-                </div>
-                <div className="col-md-9 col-md-offset-2 col-sm-8 col-sm-offset-2">
-                    <ul className="nav nav-pills search-entity-types">
-                        <li className="">
-                            <Link to="/environments">
-                                <i className="fa fa-sitemap"/>&nbsp;&nbsp;Environments&nbsp;&nbsp;</Link>
-                        </li>
-                        <li>
-                            <Link to="/applications">
-                                <i className="fa fa-cube"/>&nbsp;&nbsp;Applications</Link>
-                        </li>
-                        <li>
-                            <Link to="/instances">
-                                <i className="fa fa-cubes"/>&nbsp;&nbsp;Instances</Link>
-                        </li>
-                        <li>
-                            <Link to="/nodes">
-                                <i className="fa fa-server"/>&nbsp;&nbsp;Nodes</Link>
-
-                        </li>
-                        <li>
-                            <Link to="/resources">
-                                <i className="fa fa-cogs"/>&nbsp;&nbsp;Resources</Link>
-                        </li>
-                    </ul>
-                    <br />
+                    <img src="images/aura-ikoner/fasit.png" className="home-brand-logo "/>
+                    <img src="images/aura-ikoner/fasit-center.png" className="home-brand-logo-center "/>
                 </div>
             </div>
-        )
+        <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <form>
+                <input
+                    type="text"
+                    className="form-control search-field-text-input"
+                    ref="searchField"
+                    placeholder={'Search for ' + context}
+                    value={searchString}
+                    onChange={(e) => dispatch(submitSearchString(context, e.target.value, 0))}
+                />
+                <button
+                    type="submit"
+                    className="search-field-button btn-grey"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        browserHistory.push("/search")
+                    }}
+                ><i className="fa fa-search"/></button>
+            </form>
+            <br />
+        </div>
+        <div className="col-md-9 col-md-offset-2 col-sm-8 col-sm-offset-2">
+            <ul className="nav nav-pills search-entity-types">
+                <li className="">
+                    <Link to="/environments">
+                        <i className="fa fa-sitemap"/>&nbsp;&nbsp;Environments&nbsp;&nbsp;</Link>
+                </li>
+                <li>
+                    <Link to="/applications">
+                        <i className="fa fa-cube"/>&nbsp;&nbsp;Applications</Link>
+                </li>
+                <li>
+                    <Link to="/instances">
+                        <i className="fa fa-cubes"/>&nbsp;&nbsp;Instances</Link>
+                </li>
+                <li>
+                    <Link to="/nodes">
+                        <i className="fa fa-server"/>&nbsp;&nbsp;Nodes</Link>
+
+                </li>
+                <li>
+                    <Link to="/resources">
+                        <i className="fa fa-cogs"/>&nbsp;&nbsp;Resources</Link>
+                </li>
+            </ul>
+            <br />
+        </div>
+    </div>
+    )
     }
 }
 
