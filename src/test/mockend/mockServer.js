e5222d8e1e44fcdeff7d1fba677df351f14e2360
matsbyfl <mats@byfuglien.net>
2017-01-18 14:33:07 +0100

    mocking application

--- src/test/mockend/mockServer.js
+++ src/test/mockend/mockServer.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const environments = require('./environmentsMock')
 const resources = require('./resourcesMock')
+const applications = require('./applicationsMock')
 const resourceTypes = require('./resourceTypesMock')
 const app = express()
 const cors  = require('cors')
 const corsConfig = {
+    "Access-Control-Allow-Origin": "http://localhost:4242",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers": "Content-type",
     "exposedHeaders": "total_count, Link"
 }
 app.use(cors(corsConfig))
 
+app.get("/applications", (req, res) => {
+    res.set('total_count', applications.length)
+    res.json(applications)
+})
 
 app.get("/environments", (req, res) => {
     res.set('total_count', environments.length)
