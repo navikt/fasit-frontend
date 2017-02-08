d1521ec31e2a7f829c56f80a05ee7bdd644d3fe2
Sundby, Frode <Frode.Sundby@nav.no>
2017-02-08 16:37:14 +0100

    Legger til filters i environmentClusters

--- src/js/components/Environments/EnvironmentClusters.js
+++ src/js/components/Environments/EnvironmentClusters.js
@@ -1,21 +1,19 @@
 import React, {Component, PropTypes} from "react"
 import {connect} from "react-redux"
 import {Link} from "react-router"
-import {
-    fetchEnvironmentClusters
-} from "../../actionCreators/environment"
+import {fetchEnvironmentClusters} from "../../actionCreators/environment"
+import Select from 'react-select'
 
 class EnvironmentClusters extends Component {
     constructor(props) {
         super(props)
-    }
-
-    componentDidMount(){
-        console.log("envcluster, component did mount");
+        this.state = {
+            cluster: "",
+            application: ""
+        }
     }
 
     componentWillReceiveProps(nextProps) {
-        console.log("envcluster, received props", nextProps);
         const {dispatch, environment} = this.props
         if (environment != nextProps.environment) {
             dispatch(fetchEnvironmentClusters(nextProps.environment))
@@ -23,26 +21,60 @@ class EnvironmentClusters extends Component {
     }
 
     render() {
-        return (this.props.isFetching) ? <i className="fa fa-spinner fa-pulse fa-2x"></i> : (
-            <div>
-                <table className="table table-hover">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.props.clusters.map(cluster => {
-                        return <tr key={cluster.id}>
-                            <td>
-                                <Link to={`/environments/${this.props.environment}/clusters/${cluster.clustername}`}>{cluster.clustername}</Link>
-                            </td>
+        const {cluster, application} = this.state
+        const {clusters} = this.props
+        const clusterNames = clusters.map(c => c.clustername)
+        const applicationNames = clusters.reduce((a,b) => a.concat(b.applications), []).map(a => a.name)
+        return this.props.isFetching ? <i className="fa fa-spinner fa-pulse fa-2x"></i> : (
+                <div>
+                    <div style={{width: 350, display:"inline-block"}}>
+                        <Select
+                            resetValue=""
+                            placeholder="Cluster"
+                            name="form-field-name"
+                            value={cluster}
+                            options={this.convertToSelectObject(clusterNames)}
+                            onChange={(e) => this.handleChange("cluster", e.value)}
+                        />
+                    </div>
+                    <div style={{width: 250, display:"inline-block", paddingLeft:20}}>
+                        <Select
+                            resetValue=""
+                            placeholder="Application"
+                            name="form-field-name"
+                            value={application}
+                            options={this.convertToSelectObject(applicationNames)}
+                            onChange={(e) => this.handleChange("application", e.value)}
+                        />
+                    </div>
+                    <table className="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
                         </tr>
-                    })}
-                    </tbody>
-                </table>
-            </div>
-        )
+                        </thead>
+                        <tbody>
+                        {this.props.clusters.map(cluster => {
+                            return <tr key={cluster.id}>
+                                <td>
+                                    <Link
+                                        to={`/environments/${this.props.environment}/clusters/${cluster.clustername}`}>{cluster.clustername}</Link>
+                                </td>
+                            </tr>
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+            )
+    }
+
+    convertToSelectObject(values) {
+        return values.map(value => {
+            return {value, label: value}
+        })
+    }
+    handleChange(field, value) {
+        this.setState({[field]: value})
     }
 }
 
