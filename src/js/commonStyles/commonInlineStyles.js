3f36831fe879c89ac15dd7a30ff83c70355cf34f
B115620 <mats.byfuglien@nav.no>
2017-05-31 12:49:33 +0200

    Using card for lifecycle view

---
+++ src/js/commonStyles/commonInlineStyles.js
@@ -0,0 +1,6 @@
+export default {
+    bold: {fontWeight: 'bold'},
+    white: {color: '#ffffff'},
+    colors: {white: '#ffffff'}
+}
+
--- src/js/components/common/Lifecycle.js
+++ src/js/components/common/Lifecycle.js
@@ -1,8 +1,12 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
 import moment from 'moment'
-import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
+import {Card, CardHeader, CardActions} from 'material-ui/Card'
+import Avatar from 'material-ui/Avatar'
 import Restore from 'material-ui/svg-icons/action/restore'
+import styles  from '../../commonStyles/commonInlineStyles'
+import {red400, green400, orange300} from 'material-ui/styles/colors'
 
 class Lifecycle extends Component {
     constructor(props) {
@@ -11,26 +15,56 @@ class Lifecycle extends Component {
     }
 
     rescueButton() {
-        return (<RaisedButton disableTouchRipple={true} label="Rescue" icon={<Restore/>} onTouchTap={this.props.rescueAction} disabled={!this.props.authorized}/>
+        return (<FlatButton
+                disableTouchRipple={true}
+                label="Rescue"
+                secondary="true"
+                labelStyle={styles.bold}
+                onTouchTap={this.props.rescueAction}
+                disabled={!this.props.authorized}/>
         )
     }
 
+    avatar() {
+        return (
+            <Avatar backgroundColor={red400} color={styles.colors.white}>
+                <Restore/>
+            </Avatar>
+            )
+    }
+
+    cardBody() {
+        const {lifecycle} = this.props
+        if(lifecycle.issue) {
+            return (
+                <CardText>
+                    See jira issue {}
+                </CardText>
+            )
+        }
+}
+
     render() {
-        const {lifecycle, jira, rescueAction, authorized} = this.props
-
-
+        const {lifecycle, jira} = this.props
 
         if (lifecycle === undefined) return null
+
         if (!this.state.visible) return null
          switch (lifecycle.status) {
             case "stopped":
                  return (
-                     <div className="alert alert-dismissible alert-info col-md-8">
-                         <button type="button" className="close" onClick={() => this.setState({visible:false})}>&times;</button>
-                         This element was stopped and will be <b>deleted</b><br />
-                        {moment(lifecycle.nextactiondate).format('ll, HH:mm')}<br />
-                         {this.rescueButton()}
-                    </div>
+                     <Card>
+                         <CardHeader
+                             title="Element stopped"
+                             subtitle={`Scheduelded for deletion on ${moment(lifecycle.nextactiondate).format('ll, HH:mm')}`}
+                             actAsExpander={true}
+                             showExpandableButton={lifecycle.issue}
+                             avatar={this.avatar()} />
+
+                         <CardActions>
+                             {this.rescueButton()}
+                         </CardActions>
+                     </Card>
                 )
             case "alerted":
                 return (
@@ -40,7 +74,7 @@ class Lifecycle extends Component {
                         {moment(lifecycle.nextactiondate).format('ll, HH:mm')}
                         <br />See <a href={`${jira}/browse/${lifecycle.issue}`}
                                      target="jira">Jira-issue</a> for more details<br />
-                        {this.rescueButton()}
+
                     </div>
                 )
             case "rescued":
