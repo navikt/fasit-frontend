47b04692447d8a6147c2dd55976be5458dab8cd8
Ida <Ida.Onshus@nav.no>
2017-08-23 13:45:04 +0200

    changed the onClick event in environmentCardHeader to onTouchTap in order to only react on click (not marking text)

--- src/js/components/Environments/EnvironmentCard.js
+++ src/js/components/Environments/EnvironmentCard.js
@@ -24,12 +24,7 @@ export default function EnvironmentCard(props) {
                             subtitle={`Environmentclass: ${environment.environmentclass}`}
                             avatar={avatar}
                             children={additionalCardInfo}
-                            onClick={() =>{
-                                let selection = window.getSelection();
-                                if(selection.toString().length === 0) {
-                                    browserHistory.push('/environments/' + environment.name)
-                                }
-                                }}
+                            onTouchTap={() => browserHistory.push('/environments/' + environment.name)}
                 />
                 <CardActions>
                     <FlatButton
