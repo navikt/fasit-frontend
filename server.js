b109e26b5a4d1fc5f742eac70ba4426c0523ad19
matsbyfl <mats@byfuglien.net>
2019-08-08 10:13:46 +0200

    fixed mockapi paths after simplifying config when moving to naiserator

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(require("webpack-hot-middleware")(compiler))
 app.use(express.static(__dirname + "/dist"))
 
 /* Useful for making mock function sleep when simulating slow apis
-* sleep(5000).then(() => {sendJson(res, resourcesMock.getResource(req.params.id))})*/
+ * sleep(5000).then(() => {sendJson(res, resourcesMock.getResource(req.params.id))})*/
 function sleep(time) {
   return new Promise(resolve => setTimeout(resolve, time))
 }
@@ -52,72 +52,72 @@ app.get("/config", (req, res) => {
   res.json(config.externalResources)
 })
 
-app.put("/mockapi/lifecycle/:entityTyp/:id", (req, res) => {
+app.put("/mockapi/v1/lifecycle/:entityTyp/:id", (req, res) => {
   console.info("lifecycle", req.params)
   res.sendStatus(201)
 })
 
-app.get("/mockapi/applications/:application", (req, res) => {
+app.get("/mockapi/v2/applications/:application", (req, res) => {
   sendJson(res, applicationsMock.getApplication(req.params.application))
 })
-app.put("/mockapi/applications/:application", (req, res) => {
+app.put("/mockapi/v2/applications/:application", (req, res) => {
   sendJson(res, applicationsMock.putApplication(req.params.application))
 })
 
-app.delete("/mockapi/applications/:application", (req, res) => {
+app.delete("/mockapi/v2/applications/:application", (req, res) => {
   applicationsMock.deleteApplication(req.params.application)
   res.sendStatus(200)
 })
 
-app.get("/mockapi/navsearch", (req, res) => {
+app.get("/mockapi/v1/navsearch", (req, res) => {
   sendJson(res, navSearchMock.getNavSearch())
 })
 
-app.get("/mockapi/search", (req, res) => {
+app.get("/mockapi/v1/search", (req, res) => {
   sendJson(res, searchMock.getSearch(req.query))
 })
 
-app.get("/mockapi/applications", (req, res) => {
+app.get("/mockapi/v2/applications", (req, res) => {
   sendJson(res, applicationsMock.getApplications())
 })
 
-app.get("/mockapi/applicationinstances", (req, res) => {
+app.get("/mockapi/v2/applicationinstances", (req, res) => {
   sendJson(res, applicationinstances.findApplicationInstance(req.query))
 })
 
-app.get("/mockapi/applicationinstances/:id", (req, res) => {
+app.get("/mockapi/v2/applicationinstances/:id", (req, res) => {
   sendJson(res, applicationinstances.getInstance(req.params.id))
 })
 
-app.get("/mockapi/applicationinstances/:id/revisions", (req, res) => {
+app.get("/mockapi/v2/applicationinstances/:id/revisions", (req, res) => {
   sendJson(res, applicationinstancesRevisionsMock.getApplicationInstanceRevisions())
 })
 
-app.get("/mockapi/applicationinstances/environment/:environment", (req, res) => {
+app.get("/mockapi/v2/applicationinstances/environment/:environment", (req, res) => {
   sendJson(res, applicationinstances.findApplicationInstanceByEnv(req.params.environment))
 })
 
-app.get("/mockapi/applicationinstances/application/:application", (req, res) => {
+app.get("/mockapi/v2/applicationinstances/application/:application", (req, res) => {
   sendJson(res, applicationinstances.findApplicationInstanceByApp(req.params.application))
 })
 
-app.get("/mockapi/applicationinstances/1/revisions/69/appconfig", (req, res) => {
+app.get("/mockapi/v2/applicationinstances/1/revisions/69/appconfig", (req, res) => {
   res.send("<this><is><real><nested><xml>69</xml></nested></real></is></this>")
 })
 
-app.get("/mockapi/environments/:name/clusters/:clustername", (req, res) => {
+app.get("/mockapi/v2/environments/:name/clusters/:clustername", (req, res) => {
   sendJson(res, environmentsMock.getCluster(req.params.name, req.params.clustername))
 })
 
-app.get("/mockapi/environments/:name/clusters/:clustername/revisions", (req, res) => {
+app.get("/mockapi/v2/environments/:name/clusters/:clustername/revisions", (req, res) => {
   sendJson(res, environmentsMock.getClusterRevisions())
 })
 
-app.get("/mockapi/environments/:name/clusters/:clustername/revisions/:revision", (req, res) => {
+app.get("/mockapi/v2/environments/:name/clusters/:clustername/revisions/:revision", (req, res) => {
   sendJson(res, environmentsMock.getClusterRevision())
 })
 
-app.get("/mockapi/environments/:name/clusters/", (req, res) => {
+app.get("/mockapi/v2/environments/:name/clusters/", (req, res) => {
   sendJson(res, environmentsMock.getClusters(req.params.name))
 })
 
@@ -125,97 +125,97 @@ app.get("/mockapi/clusters/:id/", (req, res) => {
   sendJson(res, environmentsMock.getCluster("u1", "bpm"))
 })
 
-app.get("/mockapi/clusters/:id/revisions", (req, res) => {
+app.get("/mockapi/v2/clusters/:id/revisions", (req, res) => {
   sendJson(res, environmentsMock.getClusterRevisions())
 })
 
-app.get("/mockapi/clusters/:id/revisions/:revision", (req, res) => {
+app.get("/mockapi/v2/clusters/:id/revisions/:revision", (req, res) => {
   sendJson(res, environmentsMock.getClusterRevision())
 })
 
-app.get("/mockapi/environments/:name", (req, res) => {
+app.get("/mockapi/v2/environments/:name", (req, res) => {
   sendJson(res, environmentsMock.getEnvironment(req.params.name))
 })
 
-app.delete("/mockapi/environments/:name", (req, res) => {
+app.delete("/mockapi/v2/environments/:name", (req, res) => {
   sendJson(res, environmentsMock.deleteEnvironment(req.params.name))
 })
 
-app.get("/mockapi/environments", (req, res) => {
+app.get("/mockapi/v2/environments", (req, res) => {
   sendJson(res, environmentsMock.findEnvironments(req.query))
 })
 
-app.post("/mockapi/environments", (req, res) => {
+app.post("/mockapi/v2/environments", (req, res) => {
   res.sendStatus(201)
 })
 
-app.get("/mockapi/resources/types/", (req, res) => {
+app.get("/mockapi/v2/resources/types/", (req, res) => {
   sendJson(res, resourceTypes)
 })
 
-app.get("/mockapi/resources", (req, res) => {
+app.get("/mockapi/v2/resources", (req, res) => {
   sendJson(res, resourcesMock.findResources(req.query))
 })
 
-app.get("/mockapi/resources/:id", (req, res) => {
+app.get("/mockapi/v2/resources/:id", (req, res) => {
   sendJson(res, resourcesMock.getResource(req.params.id))
 })
 
-app.get("/mockapi/resources/:id/revisions", (req, res) => {
+app.get("/mockapi/v2/resources/:id/revisions", (req, res) => {
   sendJson(res, resourceRevisionMock.getResourceRevisions(req.params.id))
 })
 
-app.get("/mockapi/resources/:id/revisions/:revision", (req, res) => {
+app.get("/mockapi/v2/resources/:id/revisions/:revision", (req, res) => {
   sendJson(res, resourceRevisionMock.getResourceRevision(req.params))
 })
 
-app.delete("/mockapi/resources/:id", (req, res) => {
+app.delete("/mockapi/v2/resources/:id", (req, res) => {
   resourcesMock.deleteresource(req.params.id)
   res.sendStatus(200)
 })
 
-app.post("/mockapi/resources", (req, res) => {
+app.post("/mockapi/v2/resources", (req, res) => {
   res.sendStatus(200)
 })
 
-app.put("/mockapi/resources/:id", (req, res) => {
+app.put("/mockapi/v2/resources/:id", (req, res) => {
   res.sendStatus(201)
 })
 
-app.get("/mockapi/nodes/types", (req, res) => {
+app.get("/mockapi/v2/nodes/types", (req, res) => {
   sendJson(res, nodesMock.types)
 })
 
-app.get("/mockapi/nodes/:hostname", (req, res) => {
+app.get("/mockapi/v2/nodes/:hostname", (req, res) => {
   sendJson(res, nodesMock.getNode(req.params.hostname))
 })
 
-app.put("/mockapi/nodes/:hostname", (req, res) => {
+app.put("/mockapi/v2/nodes/:hostname", (req, res) => {
   sendJson(res, nodesMock.putNode(req.params.hostname))
 })
 
-app.post("/mockapi/nodes/:hostname", (req, res) => {
+app.post("/mockapi/v2/nodes/:hostname", (req, res) => {
   sendJson(res, nodesMock.postNode(req.params.hostname))
 })
 
-app.delete("/mockapi/nodes/:hostname", (req, res) => {
+app.delete("/mockapi/v2/nodes/:hostname", (req, res) => {
   nodesMock.deleteNode(req.params.hostname)
   res.sendStatus(200)
 })
 
-app.get("/mockapi/nodes/:hostname/revisions", (req, res) => {
+app.get("/mockapi/v2/nodes/:hostname/revisions", (req, res) => {
   sendJson(res, nodeRevisionsMock.getNodeRevisions(req.params.hostname))
 })
 
-app.get("/mockapi/nodes/:hostname/revisions/:revision", (req, res) => {
+app.get("/mockapi/v2/nodes/:hostname/revisions/:revision", (req, res) => {
   sendJson(res, nodeRevisionsMock.getNodeRevision(req.params))
 })
 
-app.get("/mockapi/nodes", (req, res) => {
+app.get("/mockapi/v2/nodes", (req, res) => {
   sendJson(res, nodesMock.getNodes())
 })
 
-app.get("/mockapi/secrets/*", (req, res) => {
+app.get("/mockapi/v2/secrets/*", (req, res) => {
   res.send("th151s4M0ck53cr3t")
 })
 
@@ -224,13 +224,13 @@ app.get("/mockapi/seramock", (req, res) => {
 })
 
 if (process.env["NODE_ENV"] === "standalone") {
-  app.post("/api/login", (req, res) => {
+  app.post("/mockapi/login", (req, res) => {
     sendJson(res, loginMock.getLogin())
   })
-  app.post("/api/logout", (req, res) => {
+  app.post("/mockapi/logout", (req, res) => {
     sendJson(res, loginMock.getLogout())
   })
-  app.get("/api/v2/currentuser", (req, res) => {
+  app.get("/mockapi/v2/currentuser", (req, res) => {
     sendJson(res, loginMock.getUser())
   })
 }
