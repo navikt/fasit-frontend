482d560bdcf9728be49401b88c4f5586b0e336eb
Rokke, Sten Ivar <Sten.Ivar.Rokke@nav.no>
2020-05-07 13:30:08 +0200

    Added issuerUrl to openidConnect resource

--- src/js/utils/resourceTypes.js
+++ src/js/utils/resourceTypes.js
@@ -131,6 +131,7 @@ export const resourceTypes = {
       textbox("agentName", "Agent name"),
       secret("password"),
       textbox("hostUrl", "Host URL"),
+      textbox("issuerUrl", "Issuer URL"),
       textbox("jwksUrl", "JWKS URL")
     ]
   },
