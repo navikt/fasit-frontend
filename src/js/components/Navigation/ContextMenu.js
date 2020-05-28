25d2b572600ee8656f446953c47ce96e0aa2c12a
matsbyfl <mats@byfuglien.net>
2020-05-28 15:01:26 +0200

    Remove icons from context meny

--- src/js/components/Navigation/ContextMenu.js
+++ src/js/components/Navigation/ContextMenu.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { styles } from "../../commonStyles/commonInlineStyles";
 
 class ContexMenu extends Component {
   constructor(props) {
@@ -24,39 +24,33 @@ class ContexMenu extends Component {
         <div>
           <ul className="nav nav-tabs nav-tab-positioning">
             <li className={this.isActive("search")}>
-              <Link to="/search">
-                <FontAwesomeIcon icon="search" />
-                &nbsp;&nbsp;Search
+              <Link to="/search" style={styles.bold}>
+                Search
               </Link>
             </li>
             <li className={this.isActive("environments")}>
-              <Link to="/environments">
-                <FontAwesomeIcon icon="sitemap" />
-                &nbsp;&nbsp;Environments
+              <Link to="/environments" style={styles.bold}>
+                Environments
               </Link>
             </li>
             <li className={this.isActive("applications")}>
-              <Link to="/applications">
-                <FontAwesomeIcon icon="cube" />
-                &nbsp;&nbsp;Applications
+              <Link to="/applications" style={styles.bold}>
+                Applications
               </Link>
             </li>
             <li className={this.isActive("instances")}>
-              <Link to="/instances">
-                <FontAwesomeIcon icon="cubes" />
-                &nbsp;&nbsp;Instances
+              <Link to="/instances" style={styles.bold}>
+                Instances
               </Link>
             </li>
             <li className={this.isActive("nodes")}>
-              <Link to="/nodes">
-                <FontAwesomeIcon icon="server" />
-                &nbsp;&nbsp;Nodes
+              <Link to="/nodes" style={styles.bold}>
+                Nodes
               </Link>
             </li>
             <li className={this.isActive("resources")}>
-              <Link to="/resources">
-                <FontAwesomeIcon icon="cogs" />
-                &nbsp;&nbsp;Resources
+              <Link to="/resources" style={styles.bold}>
+                Resources
               </Link>
             </li>
           </ul>
