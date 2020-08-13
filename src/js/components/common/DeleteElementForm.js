d549af2fe4be870a404ac8485152c09e519dce42
matsbyfl <mats@byfuglien.net>
2020-08-13 14:04:04 +0200

    remove commented out code

--- src/js/components/common/DeleteElementForm.js
+++ src/js/components/common/DeleteElementForm.js
@@ -1,31 +1,9 @@
 import React from "react"
-//import Button from '@material-ui/core/Button';
-//import {styles} from "../../commonStyles/commonInlineStyles";
-//import Dialog from '@material-ui/core/Dialog';
 import Modal from "react-bootstrap/Modal"
 import Button from "react-bootstrap/Button"
 
 export default function DeleteElement(props) {
   const { displayDeleteForm, id, onClose, onSubmit } = props
-  /*const actions = [
-        <Button
-        size="small"
-            disableTouchRipple={true}
-            style={styles.flatButton}
-            label="Ok"
-            onClick={onSubmit}/>,
-        <Button
-        size="small"
-        disableRipple={true}
-            style={styles.flatButton}
-            label="Cancel"
-            onClick={onClose}/>
-    ]*/
-
-  /*return (
-        <Dialog actions={actions} open={displayDeleteForm} modal={true} onRequestClose={onClose} title="Confirm delete">
-            Are you sure you want to delete {id}?
-        </Dialog>)*/
 
   return (
     <Modal animation={false} show={displayDeleteForm} onHide={onClose} centered>
