fa7ca62193500223261dc154fc029bac8d238838
matsbyfl <mats@byfuglien.net>
2020-08-21 13:15:03 +0200

    Use snackbar from react-bootstrap and remove material-ui component

--- src/js/components/common/SubmitFormStatus.js
+++ src/js/components/common/SubmitFormStatus.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
-import Snackbar from "@material-ui/core/Snackbar"
+import Toast from "react-bootstrap/Toast"
 import { resetFormStatus } from "../../actionCreators/common"
 
 class SubmitFormStatus extends Component {
@@ -10,14 +10,24 @@ class SubmitFormStatus extends Component {
   }
 
   render() {
-    const { form } = this.props
+    const { form, dispatch } = this.props
     return (
-      <Snackbar
-        open={form.displaySnackbar}
-        message="Success"
-        autoHideDuration={3000}
-        onClose={() => this.props.dispatch(resetFormStatus())}
-      />
+      <Toast
+        onClose={() => dispatch(resetFormStatus())}
+        show={form.displaySnackbar}
+        style={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          backgroundColor: "#72bd72",
+          borderColor: "#3c763d",
+          color: "#333",
+        }}
+        delay={2000}
+        autohide
+      >
+        <Toast.Body>Success</Toast.Body>
+      </Toast>
     )
   }
 }
