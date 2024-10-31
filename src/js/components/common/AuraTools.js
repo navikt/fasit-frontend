a93937a12c7bb85aab306ae2e1c4a7ad77d40f18
Håvard Tronhus <46346413+havtro@users.noreply.github.com>
2024-10-31 12:49:43 +0100

    Fix urls

--- src/js/components/common/AuraTools.js
+++ src/js/components/common/AuraTools.js
@@ -4,7 +4,7 @@ import { Popover } from "react-bootstrap"
 export function AuraTools() {
   return (
     <Popover title="AURA tools" id="apps">
-      <a href="https://basta.adeo.no" target="Basta">
+      <a href="https://basta.intern.nav.no/" target="Basta">
         <div className="app-container">
           <div className="app-icon">
             <img src="/images/aura-ikoner/basta.png" className="app-icon" />
@@ -12,7 +12,7 @@ export function AuraTools() {
           <div className="app-label">Basta</div>
         </div>
       </a>
-      <a href="https://vera.adeo.no" target="Vera">
+      <a href="https://vera.intern.nav.no" target="Vera">
         <div className="app-container">
           <div className="app-icon">
             <img src="/images/aura-ikoner/vera.png" className="app-icon" />
